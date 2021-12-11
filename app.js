fee4ca611d62a7abdf10341ff52200c1f6f1218e
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2021-12-10 23:47:58 -0600

    added services

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ let navbar = document.querySelector('.header .navbar');
 
 menu.onclick = () =>{
   menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active')
+  navbar.classList.toggle('active');
 };
 
 window.onscroll = () =>{
   menu.classList.remove('fa-times');
   navbar.classList.remove('active')
+
 };
 
 
@@ -17,5 +18,12 @@ let themeBtn = document.querySelector('#theme-btn');
 
 themeBtn.onclick = () => {
   themeBtn.classList.toggle('fa-sun');
+
+  if(themeBtn.classList.contains('fa-sun')){
+    document.body.classList.add('active');
+    document.querySelector('.box').classList.add('active');
+}else{
+    document.body.classList.remove('active');
+}
 }
 
