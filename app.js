f8e7948520db2b1508d17edb0f072cbd687f4ea3
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2021-12-13 22:04:56 -0600

    added more sections

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ menu.onclick = () =>{
   navbar.classList.toggle('active');
 };
 
-window.onscroll = () =>{
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active')
-
-};
-
-
 //theme btn//
 let themeBtn = document.querySelector('#theme-btn');
 
@@ -26,4 +19,11 @@ themeBtn.onclick = () => {
     document.body.classList.remove('active');
 }
 }
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active')
+
+};
+
 
